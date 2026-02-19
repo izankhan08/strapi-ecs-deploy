0967a00bdee368dfab451f00d3931fe607d22433
Mohammad Izan Khan <98902714+izankhan08@users.noreply.github.com>
2026-02-19 11:20:53 +0530

    Create ecs-service.tf

---
+++ terraform/ecs-service.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "ecs_sg" {
+  name   = "strapi-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "strapi_task" {
+  family                   = "strapi-task"
+  network_mode             = "bridge"
+  requires_compatibilities = ["EC2"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = "${data.aws_ecr_repository.strapi_repo.repository_url}:latest"
+      essential = true
+      portMappings = [{
+        containerPort = 1337
+        hostPort      = 1337
+      }]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+}
